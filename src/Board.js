b64ea1b3dedf5f0316aa365e2507ec75e620e9f1
Owen Dismuke <owen.dismuke@gmail.com>
2015-07-08 17:00:24 -0700

    Completed board.

--- src/Board.js
+++ src/Board.js
@@ -131,12 +131,31 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var context = this;
+      var count = 0;
+
+      context.rows().forEach(function(row, rowIndex){
+        if (context._isInBounds(rowIndex, majorDiagonalColumnIndexAtFirstRow)) {
+          if (row[majorDiagonalColumnIndexAtFirstRow] === 1) {
+            count++;
+          }
+        }
+        majorDiagonalColumnIndexAtFirstRow++;
+      });
+
+      return count > 1;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var context = this;
+
+      return _.range(context.get('n')).reduce(function(prev,curr){
+        if (!prev) {
+          return context.hasMajorDiagonalConflictAt(curr);
+        }
+        return prev;
+      }, false);
     },
 
 
@@ -146,12 +165,31 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var context = this;
+      var count = 0;
+
+      context.rows().forEach(function(row, rowIndex){
+        if (context._isInBounds(rowIndex, minorDiagonalColumnIndexAtFirstRow)){
+          if (row[minorDiagonalColumnIndexAtFirstRow] === 1) {
+            count++;
+          }
+        }
+        minorDiagonalColumnIndexAtFirstRow--;
+      });
+
+      return count > 1;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var context = this;
+
+      return _.range(context.get('n')).reduce(function(prev,curr){
+        if (!prev) {
+          return context.hasMinorDiagonalConflictAt(curr);
+        }
+        return prev;
+      }, false);
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
